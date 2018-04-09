2f9b501b684cc9954c5432491f90315411e9a117
maarten <maarten.brysbaert@student.howest.be>
2018-04-09 12:29:46 +0200

    school vrijdag

---
+++ Workwork/Workwork.Core/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,118 @@
+﻿using MvvmCross.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workwork.Core.Services;
+using Workwork.Functions.Models;
+
+namespace Workwork.Core.ViewModels
+{
+    public class RegisterViewModel : MvxViewModel
+    {
+        private IWorkService _workService;
+        public RegisterViewModel(IWorkService workService)
+        {
+            _workService = workService;
+            _account = new Account();
+        }
+
+        private Account _account;
+        public Account Account
+        {
+            get { return _account; }
+            set
+            {
+                _account = value;
+                RaisePropertyChanged(() => Account);
+                CheckIfUsernameExists();
+            }
+        }
+
+        private bool _usernameExists;
+        public bool UsernameExists
+        {
+            get
+            {
+                if (Account.UserName == null)
+                {
+                    return false;
+                }
+                return _usernameExists;
+            }
+            set
+            {
+                _usernameExists = value;
+            }
+        }
+
+        private bool _passwordsMatch;
+
+        public bool PasswordsMatch
+        {
+            get
+            {
+                if (Account.Password == null)
+                {
+                    return true;
+                }
+                return _passwordsMatch;
+            }
+            set { _passwordsMatch = value; }
+        }
+
+
+        private string _repeatPassword;
+        public string RepeatPassword
+        {
+            get { return _repeatPassword; }
+            set
+            {
+                _repeatPassword = value;
+                RaisePropertyChanged(() => RepeatPassword);
+            }
+        }
+
+        public void CheckPasswords()
+        {
+            if (Account.Password != null)
+            {
+                if (Account.Password == RepeatPassword)
+                {
+                    PasswordsMatch = true;
+                }
+                else
+                {
+                    PasswordsMatch = false;
+                }
+            }
+        }
+
+        public async void CheckIfUsernameExists()
+        {
+            if (Account.UserName != null)
+            {
+                UsernameExists = await _workService.UserNameExists(Account.UserName);
+            }
+        }
+
+        public void CreateAccount()
+        {   
+            //to do: checked of velden zijn ingevuld
+            if (true)
+            {
+                _workService.AddAccount(Account);
+            }
+        }
+
+        public MvxCommand AddAccount
+        {
+            get
+            {
+                return new MvxCommand(() => CreateAccount());
+            }
+        }
+
+    }
+}
---
+++ Workwork/Workwork.iOS/Views/RegisterView.designer.cs
@@ -0,0 +1,74 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Workwork.iOS
+{
+    [Register ("RegisterView")]
+    partial class RegisterView
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnRegister { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField lblFirstName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField lblLastName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField lblPassword { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField lblRepeatPassword { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField lblUsername { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnRegister != null) {
+                btnRegister.Dispose ();
+                btnRegister = null;
+            }
+
+            if (lblFirstName != null) {
+                lblFirstName.Dispose ();
+                lblFirstName = null;
+            }
+
+            if (lblLastName != null) {
+                lblLastName.Dispose ();
+                lblLastName = null;
+            }
+
+            if (lblPassword != null) {
+                lblPassword.Dispose ();
+                lblPassword = null;
+            }
+
+            if (lblRepeatPassword != null) {
+                lblRepeatPassword.Dispose ();
+                lblRepeatPassword = null;
+            }
+
+            if (lblUsername != null) {
+                lblUsername.Dispose ();
+                lblUsername = null;
+            }
+        }
+    }
+}
